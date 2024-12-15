00d2290789f97f1eaabd7f754640015651829e80
danielluccio <daniel130877@gmail.com>
2024-12-15 13:24:18 -0300

    Script para Pares na Main.py

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+
+import os
+import time
+
 def imprimirMensagem():
     mensagem = str(input("Digite uma mensagem: "))
     return mensagem
 
 imprimindo = imprimirMensagem()
 print(imprimindo)
+
+
+time.sleep(1.8)
+os.system("cls")
+
+def retornaPar():
+    empty_list = []
+    entrada = int(input("Digite um valor inteiro: "))
+    if entrada % 2 == 0:
+        empty_list.append(entrada)
+        return empty_list
+    else:
+        return f"Entrada: {entrada} -> Impar"
+    
+
+pares = retornaPar()
+print(pares)
